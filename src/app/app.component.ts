5df9093760d6e99e9ef0f8c5acbfb3e7fe377426
merlinpratheesh <merlinpratheesh@gmail.com>
2020-12-24 10:48:32 +0530

    Loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject , Subscription,Observable } from 'rxjs';
 import firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { UserdataService } from './service/userdata.service';
-import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
-import { withLatestFrom, map, switchMap,filter,tap } from 'rxjs/operators';
+import { projectVariables,projectControls, UserdataService, MainSectionGroup,TestcaseInfo,userProfile,projectFlags } from './service/userdata.service';
+import { map, switchMap,filter,take,startWith } from 'rxjs/operators';
+import { AngularFirestoreDocument } from '@angular/fire/firestore';
+import { doc } from 'rxfire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ProjectDevelopmentTool';
   subjectauth = new BehaviorSubject(undefined);
   subjectonline = new BehaviorSubject(undefined);
@@ -20,12 +23,50 @@ export class AppComponent implements OnInit {
   myauth;
   myonline;
   myonlineAfterAuth;
+  getSectionsSubscription:Subscription;
+  getSectionsBehaviourSub= new BehaviorSubject(undefined);
+  Sections;
+  SectionTc;
+  getSections = (MainAndSubSectionkeys: AngularFirestoreDocument<MainSectionGroup>) => {
+    if(this.getSectionsSubscription !== undefined){
+      this.getSectionsSubscription.unsubscribe();
+    }
+    this.getSectionsSubscription= MainAndSubSectionkeys.valueChanges().subscribe((val: any) => {
+      console.log('val',val.MainSection);
+      this.getSectionsBehaviourSub.next(val.MainSection);
+    });
+    return this.getSectionsBehaviourSub;
+  };
+  loadFirstPageTcSub:Subscription;
+  getTestcasesSubscription:Subscription;
+  getTestcasesBehaviourSub= new BehaviorSubject(undefined);
+getTestcases = (TestcasList: AngularFirestoreDocument<TestcaseInfo>) => {    
+    if(this.getTestcasesSubscription !== undefined){
+      this.getTestcasesSubscription.unsubscribe();
+    }
+    this.getTestcasesSubscription= TestcasList.valueChanges().subscribe((val: any) => {
+      let arrayeverse=val;
+      if(val !== undefined){
+        console.log('val',val.testcase);    
+        if(val.testcase === null)    {
+          this.getTestcasesBehaviourSub.complete()
+        }
+        arrayeverse.testcase= val.testcase.reverse();
+      }else{
+      }
+      this.getTestcasesBehaviourSub.next(arrayeverse);
+    });
+    return this.getTestcasesBehaviourSub;
+  };
+
+ 
+
   getObservableauthState = (authdetails: Observable<firebase.User>) => {
 
     if(this.getObservableauthStateSub !== undefined){
       this.getObservableauthStateSub.unsubscribe();
     }
-    this.getObservableauthStateSub= authdetails.subscribe((val: any) => {    
+    this.getObservableauthStateSub= authdetails.subscribe((val: any) => {     
      this.subjectauth.next(val);
     });
     return this.subjectauth;
@@ -39,10 +80,68 @@ export class AppComponent implements OnInit {
     return this.subjectonline;
   };
   isOnline$: Observable<boolean>;
+  loadfirstPageKeysSub:Subscription;
+
+  myprojectFlags: projectFlags = {
+    testcasesInSubmenu: undefined,//show add or New Testcase based on number of testcases in subsection
+    showPaymentpage: undefined,//for expired user-remove it
+    firstTestcaseEdit: false,//showditutton
+    showEditTcButton: false,
+    homeNewProject: false,
+    homeCurrentProject: false,
+    editDeleteProject: false,
+    editModifyProject: undefined,
+    editAddMainsec: false,
+    editDeleteMainsec: false,
+    editVisibility: false,
+    editAddSubSec: false,
+    editDeleteSubsec: false
+  };
+
+  myuserProfile: userProfile = {
+    userAuthenObj: undefined,
+    projectOwner: undefined,
+    projectLocation: undefined,
+    projectName: undefined,
+    membershipType: undefined,
+    endMembershipValidity: undefined,
+    mainsubsectionKeys: undefined,
+    publicProjectData: undefined,
+    ownPublicprojectData: undefined,
+    MainSectionData: undefined,
+    SubSectionData: undefined,
+    ownPublicproject: []
+  };
+  myprojectControls: projectControls = {
+    subsectionkeysControl: new FormControl(null, Validators.required),
+    testcaseInfoControl: new FormControl(),
+    createTestcaseControl: new FormControl(),
+    publicprojectControl: new FormControl(null, Validators.required),
+    ownPublicprojectControl: new FormControl(null, Validators.required),
+    editMainsectionGroup: this.fb.group({
+      editMainsectionControl: ''
+    }),
+    visibilityMainsectionGroup: this.fb.group({
+      editVisibilityControl: [{ value: false, disabled: false }]
+    }),
+    editSubsectionGroup: this.fb.group({
+      editSubsectionControl: ''
+      
+    })
+  };
+  myprojectVariables: projectVariables = {
+    testcaseInfodata: undefined,
+    initialMainSection: undefined,
+    lastSavedVisibility:false,
+    modifiedKeysDb:undefined,
+    editProjectkeysSaved: undefined
+  }
   constructor(
     public afAuth: AngularFireAuth,
-    public developmentservice:UserdataService
-
+    public developmentservice:UserdataService,
+    private db: AngularFirestore,    
+    public testerApiService: UserdataService,
+    public fb: FormBuilder
     ) {
       this.myauth = this.getObservableauthState(this.afAuth.authState);
       this.myonline = this.getObservableonine(this.developmentservice.isOnline$);
@@ -50,15 +149,126 @@ export class AppComponent implements OnInit {
         switchMap((onlineval:any)=>{
           return this.myauth.pipe(    
             filter(authstat=> authstat !== undefined),        
-            map((authval:any)=> {
+            map((myauthentication:firebase.User)=> {
+              if(myauthentication === null){// loggoff
+              }else{ //logged In            
               //read tc
+              this.myuserProfile.userAuthenObj = myauthentication;
+              this.myuserProfile.projectLocation= 'projectList/DemoProjectKey';
+              this.testerApiService.docExists(this.myuserProfile.userAuthenObj.uid).then(success=>{
+                if(success === undefined){
+                  const nextMonth: Date = new Date();
+                  nextMonth.setMonth(nextMonth.getMonth() + 1);
+                  const newItem = {
+                    MembershipEnd: nextMonth.toDateString(),
+                    MembershipType: 'Demo',
+                    projectLocation: '/projectList/DemoProjectKey',
+                    projectOwner: true,
+                    projectName: 'Demo'
+                  };
+          
+                  this.db.doc<any>('myProfile/' + this.myuserProfile.userAuthenObj.uid).set(newItem);
+                  this.myuserProfile.projectLocation = '/projectList/DemoProjectKey';
+                  //write new record
+                  this.myuserProfile.projectOwner = true;
+                  this.myuserProfile.projectName = 'Demo';
+                  this.myuserProfile.projectLocation = '/projectList/DemoProjectKey';
+                  this.myuserProfile.membershipType = 'Demo';
+                  this.myuserProfile.endMembershipValidity = new Date(nextMonth.toDateString());
+                  //other opions here for new User
+                  this.myprojectFlags.showPaymentpage = false;
+                }
+              });
+              this.loadFirstPageKeys();
               //read keys
+              this.loadFirstPageTc();
+            }
               return onlineval;      
           }))
         })        
       );
     }
-    ngOnInit() {
+
+    loadFirstPageKeys(){
+      console.log('this.loadfirstPageKeysSub', this.loadfirstPageKeysSub);
+      if(this.loadfirstPageKeysSub !== undefined){
+        this.loadfirstPageKeysSub.unsubscribe();
+      }
+      this.loadfirstPageKeysSub= doc(this.db.firestore.doc('/myProfile/' + this.myuserProfile.userAuthenObj.uid))
+      .pipe(take(1),
+         map((profileData:any)=>{
+        if (profileData.data() !== undefined) {//norecords
+          if (new Date(profileData.data().MembershipEnd).valueOf() < new Date().valueOf()) {
+            if (profileData.data().MembershipType === 'Demo') {//expired
+              this.myuserProfile.projectOwner = false;//cannot add tc
+              this.myuserProfile.projectName = 'Demo';
+              this.myuserProfile.projectLocation = '/projectList/DemoProjectKey';
+              this.myuserProfile.membershipType = 'Demo';
+              this.myuserProfile.endMembershipValidity = new Date(profileData.data().MembershipEnd);
+              this.myprojectFlags.showPaymentpage = true;// show only payments Page
+            } else {//expired member
+              const nextMonth: Date = new Date();
+              nextMonth.setMonth(nextMonth.getMonth() + 1);
+              const newItem = {
+                MembershipEnd: nextMonth.toDateString(),
+                MembershipType: 'Demo',
+                projectLocation: '/projectList/DemoProjectKey',
+                projectOwner: true,
+                projectName: 'Demo'
+              };
+              this.db.doc<any>('myProfile/' + this.myuserProfile.userAuthenObj.uid).set(newItem);
+              this.myuserProfile.projectOwner = true;
+              this.myuserProfile.projectName = 'Demo';
+              this.myuserProfile.projectLocation = '/projectList/DemoProjectKey';
+              this.myuserProfile.membershipType = 'Demo';
+              this.myuserProfile.endMembershipValidity = new Date(nextMonth.toDateString());
+              this.myprojectFlags.showPaymentpage = false;
+            }  
+          } else {//start normal
+            this.myuserProfile.projectName = profileData.data().projectName;
+            this.myuserProfile.projectOwner = profileData.data().projectOwner;
+            this.myuserProfile.projectLocation = profileData.data().projectLocation;
+            this.myuserProfile.membershipType = profileData.data().MembershipType;
+            this.myuserProfile.endMembershipValidity = new Date(profileData.data().MembershipEnd);
+            this.myprojectFlags.showPaymentpage = false;
+          }//end normal
+        }//end demo/Member        
+      this.Sections= this.getSections(this.db.doc(this.myuserProfile.projectLocation));
+      })//end map-profileData
+      ).subscribe(_=>{
+
+      });
+    }
+    loadFirstPageTc(){
+      let localProjectLocation = '';
+      if(this.loadFirstPageTcSub !== undefined){
+        this.loadFirstPageTcSub.unsubscribe();
+      }
+      this.loadFirstPageTcSub= this.myprojectControls.subsectionkeysControl.valueChanges
+      .pipe(startWith({value: '', groupValue: ''}),
+        map((selection:any)=>{
+          console.log('selection',selection);
+        if(!selection || selection.groupValue === ''){
+          this.myprojectVariables.initialMainSection='SubSection';
+          this.SectionTc=null;
+          this.myprojectFlags.showEditTcButton = false;
+        }else{
+          this.myprojectVariables.initialMainSection=selection.groupValue;
+          this.myprojectFlags.showEditTcButton = false;
+          
+          if (this.myuserProfile.projectName === 'Demo' ) {
+            localProjectLocation = 'projectList/' + this.myuserProfile.userAuthenObj.uid;
+          } else {
+            localProjectLocation = '/' + this.myuserProfile.projectName + '/' + selection.groupValue + '/items/' + selection.value;
+          }  
+          this.SectionTc = this.getTestcases(this.db.doc(localProjectLocation));
+        }
+       
+      })
+        
+      ).subscribe(_=>{
+  
+      });
     }
 
 }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -3,8 +3,78 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 import { Observable } from 'rxjs';
 import { of, merge, fromEvent } from 'rxjs';
-import { map} from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map, first } from 'rxjs/operators';
+import { FormControl,FormGroup} from '@angular/forms'
 
+export interface projectFlags
+{    
+    testcasesInSubmenu:boolean;//show add or New Testcase based on number of testcases in subsection
+    showPaymentpage:boolean;//for expired user-remove it
+    firstTestcaseEdit:boolean;
+    showEditTcButton:boolean;
+    homeNewProject:boolean;
+    homeCurrentProject:boolean;
+    editDeleteProject:boolean;
+    editModifyProject:boolean;
+    editAddMainsec:boolean;
+    editDeleteMainsec:boolean;
+    editVisibility:boolean;//visibility button
+    editAddSubSec:boolean;
+    editDeleteSubsec:boolean;
+    
+}
+export interface userProfile { 
+  userAuthenObj: firebase.User,//Receive User obj after login success
+  projectLocation?:string;//Demo or User public project ref
+  projectOwner?:boolean;
+  projectName?:string
+  membershipType?:string;
+  endMembershipValidity?:Date;
+  mainsubsectionKeys?: Observable<MainSectionGroup[]>;
+  publicProjectData?: Observable<string[]>;
+  ownPublicprojectData?: Observable<string[]>;
+  MainSectionData?: Observable<string[]>;
+  SubSectionData?: string[];
+  ownPublicproject?: string[];
+ }
+
+export interface SubSection {
+  viewvalue: string;
+}
+
+export interface TestcaseInfo{
+  heading: string;//Heading in testcase list
+  subHeading:string;//Sub-Heading in testcase list
+  description: string;//Description in testcase view
+  linktoTest: string;//stackblitzLink in testcase edit/doubleclick
+}
+
+export interface MainSectionGroup {
+  disabled: boolean;
+  name: string;
+  section: SubSection[];
+}
+
+export interface projectControls{
+  subsectionkeysControl: FormControl;//1-Keys come from db and user sub-sec selection will load a doc from demo or public proj
+  testcaseInfoControl: FormControl; //Displays the selected Testcase details
+  createTestcaseControl: FormControl;//User enters a test case name
+  publicprojectControl: FormControl;//1-User selects a public project    
+  ownPublicprojectControl: FormControl;//1-User selects own public project
+  editMainsectionGroup: FormGroup;// user selects a Main section key
+  visibilityMainsectionGroup:FormGroup,
+  editSubsectionGroup: FormGroup;  // user selects a Sub section key
+}
+export interface projectVariables
+{
+    initialMainSection?:string;
+    viewSelectedTestcase?:TestcaseInfo;
+    testcaseInfodata?: Observable<TestcaseInfo[]>;
+    modifiedKeysDb?:TestcaseInfo[];
+    editProjectkeysSaved:MainSectionGroup[];
+    lastSavedVisibility:boolean;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +84,8 @@ export class UserdataService {
   isOnline$: Observable<boolean>;
 
   constructor(
-    public auth: AngularFireAuth
-  ) {
+    public auth: AngularFireAuth,private db: AngularFirestore
+  ) { 
     this.isOnline$ = merge(
       of(null),
       fromEvent(window, 'online'),
@@ -33,5 +103,8 @@ export class UserdataService {
   }
   logout() {
     return this.auth.signOut();
+  }
+  docExists(uid: string) {
+    return this.db.doc(`myProfile/${uid}`).valueChanges().pipe(first()).toPromise();
   }
 }
